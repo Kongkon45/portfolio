381aeff84418e56887ec793bad1341ef92a6feb5
Kongkon Jowarder <kongkon4545@gmail.com>
2024-12-28 11:03:02 +0600

    about me section add

--- src/app/components/home/home.jsx
+++ src/app/components/home/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero';
 import ParticlesBackground from './Particles/Particles';
+import About from './About/About';
 
 const HomePageAllComponents = () => {
     return (
         <div>
             <ParticlesBackground/>
             <Hero/>
+            <About/>
         </div>
     );
 };
